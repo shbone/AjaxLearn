2ff3c94c5dc641da64f6e8ae4c323af1f8b9bf1d
shbone <598924626@qq.com>
2023-02-05 11:53:51 +0800

    第三次提交

---
+++ express_schema/ajax案例/2-post-express.js
@@ -0,0 +1,29 @@
+// 1.引入express
+// const { response } = require('express');
+const express= require('express');
+
+// 2. 实例化
+const app =express();
+// 3. 创建路由规则
+app.get("/server",(request,response) =>{
+    // 允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+    //设置相应
+    response.send('Ajax Test');
+    
+    
+});
+
+app.post("/server",(request,response) =>{
+    // 允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+    //设置相应
+    response.send('Ajax Test Post');
+    
+    
+});
+
+// 4. 监听端口服务
+app.listen(8000,()=>{
+    console.log("service start,8000 port监听中！");
+})
