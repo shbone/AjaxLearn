8ce5696e580fa37bb351368dd82511ee0eea6cc3
shbone <598924626@qq.com>
2023-02-18 19:51:53 +0800

    2.18提交

--- README.md
+++ README.md
@@ -5,7 +5,12 @@
 - 1-get.html  Ajax发送get请求
 - 2-post-express.js Ajax发送post请求
 - 3-json 发送get请求接受json格式数据
+- 4-huancun.html 设置?t=Date.now() 区别不同时间的请求
+  ![avatar](images\微信截图_20230212140000.png)
+- 5-网络超时问题
+- 6-ajax get/post请求方法
 ### 后端
 - express.js express框架后端
 - 2-express.js 添加针对post请求的修改
 - 3-get-json-express.js 返会json格式数据
+- 4-huancun-express.js 处理缓存
---
+++ express_schema/express.js
@@ -0,0 +1,56 @@
+// 1.引入express
+// const { response } = require('express');
+const express= require('express');
+
+// 2. 实例化
+const app =express();
+// 3. 创建路由规则
+app.get("/server",(request,response) =>{
+    // 允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+    //设置相应
+    response.send('Ajax Test');
+    
+    
+});
+
+//网络延时
+app.all("/delay",(request,response) =>{
+    // 允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+    //设置相应
+    // response.send('Ajax dalay');
+    setTimeout(() => {
+        response.send('Ajax delay time')
+    }, 3000);
+    
+    
+});
+
+app.all("/jquery-server",(request,response) =>{
+    // 允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+    //设置相应
+    // response.send('Ajax dalay');
+    const data = JSON.stringify({data:'JQuery send'});
+    response.send(data);
+    
+    
+});
+
+app.all("/axios-server",(request,response) =>{
+    // 允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+    //设置相应
+    // response.send('Ajax dalay');
+    const data = JSON.stringify({data:'Axios send'});
+    response.send(data);
+    
+    
+});
+
+
+// 4. 监听端口服务
+app.listen(8000,()=>{
+    console.log("service start,8000 port监听中！");
+})
